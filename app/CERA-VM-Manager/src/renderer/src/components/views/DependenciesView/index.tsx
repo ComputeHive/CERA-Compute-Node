9e178a3d4ec8a08a05461b33329297134859a4ae
Ahmed <ahmed125.am13@gmail.com>
2026-02-14 23:15:26 +0200

    Adding SSE Command execution

--- app/CERA-VM-Manager/src/renderer/src/components/views/DependenciesView/index.tsx
+++ app/CERA-VM-Manager/src/renderer/src/components/views/DependenciesView/index.tsx
@@ -1,14 +1,17 @@
 import Button from '@renderer/components/ui/Button'
 import Loader from '@renderer/components/ui/Loader'
+import { LogViewer } from '../LogViewer'
 
 export default function DependenciesView(): React.JSX.Element {
   return (
-    <section className="relative flex w-3/4 h-3/4 bg-surface my-16 p-16">
+    <section className="relative flex flex-col items-center gap-8 w-3/4 h-3/4 bg-surface my-16 p-16">
       <article className="flex items-start justify-center gap-4  ">
         <Loader />
         <h1 className="text-3xl font-bold">Check System Prerequisites</h1>
       </article>
-      <article> {/** This 'll contain the component of Checked dependencies */}</article>
+      <article>
+        <LogViewer />
+      </article>
       <article className="absolute bottom-8 right-8">
         <Button>Next</Button>
       </article>
--- app/CERA-VM-Manager/src/renderer/src/lib/utils.ts
+++ app/CERA-VM-Manager/src/renderer/src/lib/utils.ts
@@ -1,6 +1,8 @@
+import { QueryClient } from '@tanstack/react-query'
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs))
 }
+export const queryClient = new QueryClient()
